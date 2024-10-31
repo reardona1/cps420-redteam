f0b2024975eb2bd9bb4dd60de5bdee35ed5a0b1c
Andrew Reardon <90805266+reardona1@users.noreply.github.com>
2024-10-31 16:52:03 -0400

    testcases test

--- src/tests/TestCases.java
+++ src/tests/TestCases.java
@@ -4,14 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCases {
-
+    /*
     @Test
     public void newBookNoISBNTest() {
         Book book1 = new Book("Test Book", "Red Team", null, 100);
         
         Assert.assertNull("ISBN should be null", book1.getIsbn());
     }
-    
+    */
+   
     @Test
     public void failingTest() {
         Assert.assertEquals("This should fail", "expected", "actual");
