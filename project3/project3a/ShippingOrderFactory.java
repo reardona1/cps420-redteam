4dea97d5742ec5fcf6f068df22fd12b198528a98
Kyle Langenderfer <langenderferk4@udayton.edu>
2024-11-25 16:40:48 -0500

    Update ShippingOrderFactory.java

--- project3/project3a/ShippingOrderFactory.java
+++ project3/project3a/ShippingOrderFactory.java
@@ -5,21 +5,21 @@ interface ShippingOrderFactory {
 }
 
 class AirShippingOrderFactory implements ShippingOrderFactory {
-	
+    @Override
     public AirShippingOrder createShippingOrder(String orderID, double weight, String destination) {
         return new AirShippingOrder(orderID, weight, destination);
     }
 }
 
 class SeaShippingOrderFactory implements ShippingOrderFactory{
-
+        @Override
 	public SeaShippingOrder createShippingOrder(String orderID, double weight, String destination) {
 		return new SeaShippingOrder(orderID, weight, destination);
 	}
 }
 
 class LandShippingOrderFactory implements ShippingOrderFactory{
-
+        @Override
 	public LandShippingOrder createShippingOrder(String orderID, double weight, String destination) {
 		return new LandShippingOrder(orderID, weight, destination);
 	}
